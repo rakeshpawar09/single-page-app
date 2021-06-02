a69ba145e09794c2d042e3e1ee159201f7a7cb06
rakeshpawar09 <82820001+rakeshpawar09@users.noreply.github.com>
2021-06-02 10:09:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 import router from "./router";
 
 (async function () {
-  // const domain = window.env.AUTH0_DOMAIN;
-  // const client_id = window.env.CLIENT_ID;
-  // const redirect_uri = window.env.APP_URL;
+  const domain = window.env.AUTH0_DOMAIN;
+  const client_id = window.env.CLIENT_ID;
+  const redirect_uri = window.env.APP_URL;
   
-  const domain = 'https://dev-izkfk8s2.eu.auth0.com';
-  const client_id = 'FbtD5voa2yRSUwqXH34wv173hXKkHR4x';
-  const redirect_uri = 'https://expenses-api';
-
   window.auth0Client = await createAuth0Client({
     domain,
     client_id,
